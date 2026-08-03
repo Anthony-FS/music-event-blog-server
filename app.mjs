fe6c91aae25f13e26502f60c0ef6e3c2df99a278
AnthonyFS <anthonyfungesmith@gmail.com>
2026-08-03 15:53:32 +0700

    feat(auth): add user and admin protection middleware, implement protected routes, and update dependencies

--- app.mjs
+++ app.mjs
@@ -1,6 +1,9 @@
+import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import postsRouter from "./routes/posts.mjs";
+import protectUser from "./middlewares/protectUser.mjs";
+import protectAdmin from "./middlewares/protectAdmin.mjs";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -27,7 +30,13 @@ app.get("/health", (req, res) => {
   res.status(200).json({ message: "OK" });
 });
 
+app.get("/protected-route", protectUser, (req, res) => {
+  res.json({ message: "This is protected content", user: req.user });
+});
 
+app.get("/admin-only", protectAdmin, (req, res) => {
+  res.json({ message: "This is admin-only content", admin: req.user });
+});
 
 app.listen(port, () => {
   console.log(`Server is running at ${port}`);
--- utils/db.mjs
+++ utils/db.mjs
@@ -1,9 +1,8 @@
 import * as pg from "pg";
-const { Pool } = pg;
+const { Pool } = pg.default;
 
 const connectionPool = new Pool({
-    connectionString: 
-    "postgresql://postgres:supabase159963@db.oydwfktroppqjdwpbzpa.supabase.co:5432/postgres"
+  connectionString: process.env.CONNECTION_STRING,
 });
 
 export default connectionPool;
